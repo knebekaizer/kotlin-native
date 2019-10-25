9e89f648556dbbfe887fa5b82564e49d1a5f85b5
Vladimir Ivanov <vladimir.d.ivanov@jetbrains.com>
2019-10-25 18:47:36 +0300

    minor

--- samples/simd/simd_lib.c
+++ samples/simd/simd_lib.c
@@ -36,16 +36,7 @@ void printVFloat(vFloat v) {
 	fprintf(stderr, "vFloat(%f, %f, %f, %f)\n", v[0], v[1], v[2], v[3]);
 }
 
-void Kotlin_Vector_set(vFloat* thiz, uint32_t index, float value) {
-	if (index < 4) {
-		((float*)thiz)[index] = value;
-	}
-}
 
-void setDouble(double x) {
-	static double f;
-	f = x;
-}
 
 #ifdef MAIN
 
